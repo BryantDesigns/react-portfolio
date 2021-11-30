73af0624b3c11d8488f0a0e624116c4b5b78b1cd
Tyler Bryant <tbryantdesignsllc@gmail.com>
2021-11-29 21:43:31 -0700

    Styled and added content to Navbar and Header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg';
 import './navbar.css'
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div>
-      Navbar
+    <div className="bryantdesigns__navbar">
+      <div className="bryantdesigns__navbar-links">
+        <div className="bryantdesigns__navbar-links_logo">
+          <img src={logo} alt="logo of the page" />
+        </div>
+        <div className="bryantdesigns__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wbryantdesigns">What is Bryant Designs?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className="bryantdesigns__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="bryantdesigns__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="bryantdesigns__navbar-menu_container scale-up-center">
+          <div className="bryantdesigns__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wbryantdesigns">What is Bryant Designs?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+          <div className="bryantdesigns__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+        </div>
+        )}
+      </div>
     </div>
   )
 }
