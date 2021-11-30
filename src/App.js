2848a03ed3c011dbc87a7d26863c05f6e763f8e0
Tyler Bryant <tbryantdesignsllc@gmail.com>
2021-11-29 20:49:07 -0700

    Added component and container folders and files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+
 const App = () => {
   return (
     <div>
-      
+      <h1>Bryant Designs</h1>
     </div>
   )
 }
 
 export default App
+
---
+++ src/containers/whatBryantDesigns/WhatBryantDesigns.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './whatbryantdesigns.css'
+const WhatBryantDesigns = () => {
+  return (
+    <div>
+      WhatBryantDesigns
+    </div>
+  )
+}
+
+export default WhatBryantDesigns
