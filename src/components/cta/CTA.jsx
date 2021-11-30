5ecc3f97de2e0eae026cddb2c29e04b0b757ddde
Tyler Bryant <tbryantdesignsllc@gmail.com>
2021-11-29 21:54:26 -0700

    Added styles and content to brand, article, CTA and feature

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import './cta.css'
-const CTA = () => {
-  return (
-    <div>
-      CTA
-    </div>
-  )
-}
+import React from 'react';
+import './cta.css';
 
-export default CTA
+const CTA = () => (
+  <div className="bryantdesigns__cta">
+    <div className="bryantdesigns__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today & start exploring the endless possibilities.</h3>
+    </div>
+    <div className="bryantdesigns__cta-btn">
+      <button type="button">Get Started</button>
+    </div>
+  </div>
+);
+
+export default CTA;
